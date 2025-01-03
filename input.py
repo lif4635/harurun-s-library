a0852b402c4e34eb8d58aa0a1693de93b7f8cb07
lif4635 <92586000+lif4635@users.noreply.github.com>
2025-01-04 02:26:59 +0900

    Create input.py

---
+++ input.py
@@ -0,0 +1,48 @@
+# input
+input = sys.stdin.readline
+II = lambda : int(input())
+MI = lambda : map(int, input().split())
+LI = lambda : list(map(int, input().split()))
+SI = lambda : input()
+LLI = lambda n : [list(map(int, input().split())) for _ in range(n)]
+LSI = lambda n : [input() for _ in range(n)]
+MI_1 = lambda : map(lambda x:int(x)-1, input().split())
+LI_1 = lambda : list(map(lambda x:int(x)-1, input().split()))
+
+def graph(n:int, m:int, dir:bool=False, index:int=-1) -> list[set[int]]:
+    edge = [set() for i in range(n+1+index)]
+    for _ in range(m):
+        a,b = map(int, input().split())
+        a += index
+        b += index
+        edge[a].add(b)
+        if not dir:
+            edge[b].add(a)
+    return edge
+
+def graph_w(n:int, m:int, dir:bool=False, index:int=-1) -> list[set[tuple]]:
+    edge = [set() for i in range(n+1+index)]
+    for _ in range(m):
+        a,b,c = map(int, input().split())
+        a += index
+        b += index
+        edge[a].add((b,c))
+        if not dir:
+            edge[b].add((a,c))
+    return edge
+
+mod, inf = 998244353, 1001001001001001001
+ordalp = lambda s : ord(s)-65 if s.isupper() else ord(s)-97
+ordallalp = lambda s : ord(s)-91 if s.isupper() else ord(s)-97
+yes = lambda : print("Yes")
+no = lambda : print("No")
+yn = lambda flag : print("Yes" if flag else "No")
+prinf = lambda ans : print(ans if ans < 1001001001001001 else -1)
+alplow = "abcdefghijklmnopqrstuvwxyz"
+alpup = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+alpall = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+URDL = {'U':(-1,0), 'R':(0,1), 'D':(1,0), 'L':(0,-1)}
+DIR_4 = [[-1,0],[0,1],[1,0],[0,-1]]
+DIR_8 = [[-1,0],[-1,1],[0,1],[1,1],[1,0],[1,-1],[0,-1],[-1,-1]]
+DIR_BISHOP = [[-1,1],[1,1],[1,-1],[-1,-1]]
+prime60 = [2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59]
