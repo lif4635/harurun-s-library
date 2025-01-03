7f14889af27f0852cbda38aa05ff26baaa4328b8
lif4635 <92586000+lif4635@users.noreply.github.com>
2025-01-04 03:07:46 +0900

    Create pi_count.py

---
+++ library/prime/pi_count.py
@@ -0,0 +1,16 @@
+import math
+def pi_count(n):
+    """
+    LucyDP : O(n^(3/4)) 
+    """
+    sqrtn = math.isqrt(n)
+    q = [n//i for i in range(1,sqrtn+1)]
+    q += [*range(q[-1]-1,0,-1)]
+    s = {i:i-1 for i in q}
+    
+    for x in range(2,sqrtn+1):
+        if s[x] > s[x-1]:
+            for m in q:
+                if m < x*x: break
+                s[m] -= s[m//x] - s[x-1]
+    return s[n]
