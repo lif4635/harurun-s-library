19983c0106bfecb9cd4787227c5f1a9a06c3771b
lif4635 <92586000+lif4635@users.noreply.github.com>
2025-01-04 19:51:11 +0900

    Update comb.py

--- library/number_theory/comb.py
+++ library/number_theory/comb.py
@@ -1,7 +1,7 @@
 mod = 998244353
 
 class Comb:
-    def __init__(self, lim:int ,mod:int = mod):
+    def __init__(self, lim:int, mod:int = mod):
         """
         mod : prime
         """
@@ -14,18 +14,18 @@ class Comb:
             self.inv[i] = -self.inv[mod%i]*(mod//i)%self.mod
             self.finv[i] = self.finv[i-1]*self.inv[i]%self.mod
     
-    def C(self,a,b):
+    def C(self, a, b):
         assert b >= 0, "The second argument is negative."
         if a < b: return 0
         if a < 0: return 0
         return self.fac[a]*self.finv[b]%self.mod*self.finv[a-b]%self.mod
     
-    def P(self,a,b):
+    def P(self, a, b):
         assert b >= 0, "The second argument is negative."
         if a < b: return 0
         if a < 0: return 0
         return self.fac[a]*self.finv[a-b]%self.mod
     
-    def H(self,a,b): return self.C(a+b-1,b)
-    def F(self,a): return self.fac[a]
-    def Fi(self,a): return self.finv[a]
+    def H(self, a, b): return self.C(a+b-1,b)
+    def F(self, a): return self.fac[a]
+    def Fi(self, a): return self.finv[a]
