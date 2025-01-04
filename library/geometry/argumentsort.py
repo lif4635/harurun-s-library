b84a1b39d512bc5893c7f7efec494f80a2dd1a88
lif4635 <92586000+lif4635@users.noreply.github.com>
2025-01-04 12:53:32 +0900

    Update argumentsort.py

--- library/geometry/argumentsort.py
+++ library/geometry/argumentsort.py
@@ -1,7 +1,6 @@
-def quick_sort(lst:list, comparision, left:int = 0, right:int = -1):
+def quick_sort(lst, comparision, left = 0, right = -1):
     if len(lst) == 0: return
-    if right == -1:
-        right %= len(lst)
+    if right == -1: right %= len(lst)
     i = left
     j = right
     pivot = (i+j)//2
@@ -16,10 +15,8 @@ def quick_sort(lst:list, comparision, left:int = 0, right:int = -1):
         i += 1
         j -= 1
     
-    if left < i - 1:
-        quick_sort(lst, comparision, left, i - 1)
-    if right > j + 1:
-        quick_sort(lst, comparision, j + 1, right)
+    if left < i-1: quick_sort(lst, comparision, left, i-1)
+    if right > j+1: quick_sort(lst, comparision, j+1, right)
 
 def arg_sort(points:list[tuple[int]]):
     """
@@ -29,10 +26,10 @@ def arg_sort(points:list[tuple[int]]):
     for x,y in points:
         if x == 0 and y == 0:
             zero.append((0,0))
-        elif y < 0 or (x == 0 and y <= 0):
+        elif y > 0 or (x > 0 and y == 0):
             yposi.append((x,y))
         else:
             ynega.append((x,y))
-    quick_sort(yposi, lambda p1,p2 : p2[0]*p1[1] - p2[1]*p1[0])
-    quick_sort(ynega, lambda p1,p2 : p2[0]*p1[1] - p2[1]*p1[0])
+    quick_sort(yposi,lambda p1,p2 : p2[0]*p1[1] - p2[1]*p1[0])
+    quick_sort(ynega,lambda p1,p2 : p2[0]*p1[1] - p2[1]*p1[0])
     return yposi+zero+ynega
