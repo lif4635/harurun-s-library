7084a4e4ece123537f6a9857317e9dc2367581ad
lif4635 <92586000+lif4635@users.noreply.github.com>
2025-01-06 06:03:06 +0900

    Update input.py

--- library/input.py
+++ library/input.py
@@ -34,7 +34,7 @@ def graph_w(n:int, m:int, dir:bool=False, index:int=-1) -> list[set[tuple]]:
 
 mod, inf = 998244353, 1001001001001001001
 ordalp = lambda s : ord(s)-65 if s.isupper() else ord(s)-97
-ordallalp = lambda s : ord(s)-91 if s.isupper() else ord(s)-97
+ordallalp = lambda s : ord(s)-39 if s.isupper() else ord(s)-97
 yes = lambda : print("Yes")
 no = lambda : print("No")
 yn = lambda flag : print("Yes" if flag else "No")
