8ad8f81dfce4d0e0f1609f39c189a7971a7b3735
lif4635 <92586000+lif4635@users.noreply.github.com>
2025-01-06 12:36:22 +0900

    Update partiton_num.py

--- library/number_theory/partiton_num.py
+++ library/number_theory/partiton_num.py
@@ -3,27 +3,15 @@ def partition_num(n:int):
     nの分割を昇順に返す
     """
     a = [1]*n
-    
+    yield a
     while len(a) != 1:
-        yield a
-        
         pre = a.pop()
         cnt = pre
-        l = len(a)
-        for i in range(l-1,-1,-1):
-            if a[-1] == pre:
-                cnt += a.pop()
-            else:
-                if cnt >= pre+1:
-                    a += [pre+1]
-                    cnt -= pre+1
-                    a += [1]*cnt
-                    break
-                else:
-                    pre = a.pop()
-                    cnt += pre
+        while a and (cnt <= pre or a[-1] == pre):
+            pre = a.pop()
+            cnt += pre
         else:
-            a = [pre+1]
+            a += [pre+1]
             cnt -= pre+1
             a += [1]*cnt
-    yield a
+        yield a
