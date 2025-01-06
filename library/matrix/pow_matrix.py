ad22ace68d0dceeec9b48a16b55db6f9c068fef3
lif4635 <92586000+lif4635@users.noreply.github.com>
2025-01-06 13:06:35 +0900

    Update pow_matrix.py

--- library/matrix/pow_matrix.py
+++ library/matrix/pow_matrix.py
@@ -1,3 +1,17 @@
+mod = 998244353
+
+def mul_matrix(A, B, mod = mod):
+    N, K, M = len(A), len(A[0]), len(B[0])
+    assert K == len(B)
+    
+    res = [[0 for _ in range(M)] for _ in range(N)]
+    for i in range(N) :
+        for j in range(K) :
+            for k in range(M) :
+                res[i][k] += A[i][j] * B[j][k] 
+                res[i][k] %= mod
+    return res
+
 def pow_matrix(mat, exp, mod = mod):
     N = len(mat)
     res = [[1 if i == j else 0 for i in range(N)] for j in range(N)]
