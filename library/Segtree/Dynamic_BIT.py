925c50dbff5472938c1e8fe6d87b1cc0dbb31e91
lif4635 <92586000+lif4635@users.noreply.github.com>
2025-01-10 01:09:13 +0900

    Create Dynamic_BIT.py

---
+++ library/Segtree/Dynamic_BIT.py
@@ -0,0 +1,30 @@
+class Dynamic_BIT:
+    def __init__(self, n):
+        self.n = n
+        self.data = dict()
+    
+    def add(self, p, x):
+        p += 1
+        while p < self.n:
+            if p in self.data:
+                self.data[p] += x
+            else:
+                self.data[p] = x
+            p += p& -p
+    
+    def sum0(self, r):
+        s = 0
+        while r > 0:
+            if r in self.data:
+                s += self.data[r]
+            r -= r& -r
+        return s
+    
+    def sum(self, l, r):
+        return self.sum0(r) - self.sum0(l)
+    
+    def get(self, p):
+        return self.sum0(p+1) - self.sum0(p)
+    
+    def __str__(self):
+        return str([self.get(i) for i in range(self.n)])
