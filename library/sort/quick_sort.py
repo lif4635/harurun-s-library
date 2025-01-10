72bf7af26b67fe42b7b76ec8a4cda622987b9741
lif4635 <92586000+lif4635@users.noreply.github.com>
2025-01-10 22:51:34 +0900

    Update quick_sort.py

--- library/sort/quick_sort.py
+++ library/sort/quick_sort.py
@@ -1,19 +1,21 @@
 def quick_sort(lst, comparision, left = 0, right = -1):
     if len(lst) == 0: return
     if right == -1: right %= len(lst)
-    i = left
-    j = right
-    pivot = (i+j)//2
-    dpivot = lst[pivot]
+    que = [(left,right)]
+    while que:
+        i,j = que.pop()
+        left,right = i,j
+        pivot = (i+j)//2
+        dpivot = lst[pivot]
 
-    while True:
-        while comparision(lst[i],dpivot) < 0: i += 1
-        while comparision(dpivot,lst[j]) < 0: j -= 1
-        if i >= j: break
+        while True:
+            while comparision(lst[i],dpivot) < 0: i += 1
+            while comparision(dpivot,lst[j]) < 0: j -= 1
+            if i >= j: break
 
-        lst[i],lst[j] = lst[j],lst[i]
-        i += 1
-        j -= 1
+            lst[i],lst[j] = lst[j],lst[i]
+            i += 1
+            j -= 1
     
-    if left < i-1: quick_sort(lst, comparision, left, i-1)
-    if right > j+1: quick_sort(lst, comparision, j+1, right)
+        if left < i-1: que.append((left, i-1))
+        if right > j+1: que.append((j+1, right))
