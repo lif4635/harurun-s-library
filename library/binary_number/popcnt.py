dea3236587c68f5bf4d3bb85f82f9553b60e2114
lif4635 <92586000+lif4635@users.noreply.github.com>
2025-01-10 14:23:04 +0900

    Update popcnt.py

--- library/binary_number/popcnt.py
+++ library/binary_number/popcnt.py
@@ -1,8 +1,7 @@
 def popcnt(n:int) -> int:
-    c=(n&0x5555555555555555)+((n>>1)&0x5555555555555555)
-    c=(c&0x3333333333333333)+((c>>2)&0x3333333333333333)
-    c=(c&0x0f0f0f0f0f0f0f0f)+((c>>4)&0x0f0f0f0f0f0f0f0f)
-    c=(c&0x00ff00ff00ff00ff)+((c>>8)&0x00ff00ff00ff00ff)
-    c=(c&0x0000ffff0000ffff)+((c>>16)&0x0000ffff0000ffff)
-    c=(c&0x00000000ffffffff)+((c>>32)&0x00000000ffffffff)
-    return c
+    x = x - ((x >>> 1) & 0x55555555);
+    x = (x & 0x33333333) + ((x >>> 2) & 0x33333333)
+    x = (x + (x >>> 4)) & 0x0f0f0f0f
+    x = x + (x >>> 8)
+    x = x + (x >>> 16)
+    return x & 0x3f
