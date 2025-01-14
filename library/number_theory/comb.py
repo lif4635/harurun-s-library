86e8b00e7f0142f5d3a2b81a941c84c2533351df
lif4635 <92586000+lif4635@users.noreply.github.com>
2025-01-14 14:45:56 +0900

    Update comb.py
    finvの計算方法を改善しました。

--- library/number_theory/comb.py
+++ library/number_theory/comb.py
@@ -1,18 +1,16 @@
-mod = 998244353
-
 class Comb:
     def __init__(self, lim:int, mod:int = mod):
         """
         mod : prime
         """
         self.fac = [1]*(lim+1)
-        self.inv = [1]*(lim+1)
         self.finv = [1]*(lim+1)
         self.mod = mod
         for i in range(2,lim+1):
             self.fac[i] = self.fac[i-1]*i%self.mod
-            self.inv[i] = -self.inv[mod%i]*(mod//i)%self.mod
-            self.finv[i] = self.finv[i-1]*self.inv[i]%self.mod
+        self.finv[lim] = pow(self.fac[lim],-1,mod)
+        for i in range(lim,2,-1):
+            self.finv[i-1] = self.finv[i]*i%self.mod
     
     def C(self, a, b):
         assert b >= 0, "The second argument is negative."
