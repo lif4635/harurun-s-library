d194e45c353d16c18fa7d8aa982c09dcede0d0c1
lif4635 <92586000+lif4635@users.noreply.github.com>
2025-01-19 02:56:39 +0900

    Update pow_matrix.py

--- library/matrix/pow_matrix.py
+++ library/matrix/pow_matrix.py
@@ -1,22 +1,50 @@
 mod = 998244353
 
-def mul_matrix(A, B, mod = mod):
-    N, K, M = len(A), len(A[0]), len(B[0])
-    assert K == len(B)
-    
-    res = [[0 for _ in range(M)] for _ in range(N)]
-    for i in range(N) :
-        for j in range(K) :
-            for k in range(M) :
-                res[i][k] += A[i][j] * B[j][k] 
+def mat_add(a, b, mod = mod):
+    assert len(a) == len(b)
+    assert len(a[0]) == len(b[0])
+    n = len(a)
+    m = len(a[0])
+    res = [[0]*m for i in range(n)]
+    for i in range(n):
+        for j in range(m):
+            res[i][j] = (a[i][j] + b[i][j])%mod
+    return res
+
+def mat_sub(a, b, mod = mod):
+    assert len(a) == len(b)
+    assert len(a[0]) == len(b[0])
+    n = len(a)
+    m = len(a[0])
+    res = [[0]*m for i in range(n)]
+    for i in range(n):
+        for j in range(m):
+            res[i][j] = (a[i][j] - b[i][j])%mod
+    return res
+
+def mat_mul(a, b, mod = mod):
+    assert len(a[0]) == len(b)
+    n = len(a)
+    k = len(a[0])
+    m = len(b[0])
+    res = [[0]*m for i in range(n)]
+    for i in range(n):
+        for j in range(k):
+            for k in range(m):
+                res[i][j] += a[i][j] * b[j][k]
                 res[i][k] %= mod
     return res
 
-def pow_matrix(mat, exp, mod = mod):
-    N = len(mat)
-    res = [[1 if i == j else 0 for i in range(N)] for j in range(N)]
-    while exp > 0 :
-        if exp&1: res = mul_matrix(res, mat, mod)
-        mat = mul_matrix(mat, mat, mod)
+def mat_inv(a, mod = mod):
+    """いつか実装します"""
+    pass
+
+def mat_pow(a, exp, mod = mod):
+    assert len(a) == len(a[0])
+    n = len(a)
+    res = [[1 if i == j else 0 for i in range(n)] for j in range(n)]
+    while exp > 0:
+        if exp&1: res = mat_mul(res, a, mod)
+        a = mat_mul(a, a, mod)
         exp >>= 1
     return res
