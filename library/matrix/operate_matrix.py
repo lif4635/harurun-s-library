e034160c50d178147e8c0458d2c201a99642a717
lif4635 <star.mido.322@gmail.com>
2025-02-01 02:42:31 +0900

    modなくした（はやくなるので）

--- library/matrix/operate_matrix.py
+++ library/matrix/operate_matrix.py
@@ -1,8 +1,8 @@
 mod = 998244353
 
-def mat_add(a, b, mod = mod):
-    assert len(a) == len(b)
-    assert len(a[0]) == len(b[0])
+def mat_add(a, b):
+    # assert len(a) == len(b)
+    # assert len(a[0]) == len(b[0])
     n = len(a)
     m = len(a[0])
     res = [[0]*m for i in range(n)]
@@ -11,9 +11,9 @@ def mat_add(a, b, mod = mod):
             res[i][j] = (a[i][j] + b[i][j])%mod
     return res
 
-def mat_sub(a, b, mod = mod):
-    assert len(a) == len(b)
-    assert len(a[0]) == len(b[0])
+def mat_sub(a, b):
+    # assert len(a) == len(b)
+    # assert len(a[0]) == len(b[0])
     n = len(a)
     m = len(a[0])
     res = [[0]*m for i in range(n)]
@@ -22,25 +22,36 @@ def mat_sub(a, b, mod = mod):
             res[i][j] = (a[i][j] - b[i][j])%mod
     return res
 
-def mat_mul(a, b, mod = mod):
+def mat_mul(a, b):
     # assert len(a[0]) == len(b)
-    res = [[0]*len(b[0]) for i in range(len(a))]
-    for i,ri in enumerate(res):
-        for k,aik in enumerate(a[i]):
-            for j,bkj in enumerate(b[k]):
-                ri[j] = (ri[j]+aik*bkj)%mod
+    n = len(a)
+    m = len(b[0])
+    res = [[0]*m for i in range(n)]
+    for i,r_i in enumerate(res):
+        for k,a_ik in enumerate(a[i]):
+            for j,b_kj in enumerate(b[k]):
+                r_i[j] = (r_i[j] + a_ik*b_kj)%mod
+    return res
+
+def mat_pow2(a):
+    n = len(a)
+    res = [[0]*n for i in range(n)]
+    for i,r_i in enumerate(res):
+        for k,a_ik in enumerate(a[i]):
+            for j,a_kj in enumerate(a[k]):
+                r_i[j] = (r_i[j] + a_ik*a_kj)%mod
     return res
 
 def mat_inv(a, mod = mod):
     """いつか実装します"""
     pass
 
-def mat_pow(a, exp, mod = mod):
-    assert len(a) == len(a[0])
+def mat_pow(a, exp):
     n = len(a)
-    res = [[1 if i == j else 0 for i in range(n)] for j in range(n)]
-    while exp > 0:
-        if exp&1: res = mat_mul(res, a, mod)
-        a = mat_mul(a, a, mod)
-        exp >>= 1
-    return res
+    res = [[int(i == j) for j in range(n)] for i in range(n)]
+    
+    d = exp.bit_length()
+    for i in range(d, -1, -1):
+        if (exp >> i) & 1: res = mat_mul(res, a)
+        if i == 0: return res
+        res = mat_pow2(res)
