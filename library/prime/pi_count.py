a48a01e2fdf3f94fb5f1aa17cab944ba064e2d29
lif4635 <star.mido.322@gmail.com>
2025-02-19 22:46:47 +0900

    なおしたよ

--- library/prime/pi_count.py
+++ library/prime/pi_count.py
@@ -1,7 +1,3 @@
-"""
-なにものかに荒らされています
-"""
-
 from math import isqrt
 
 def pi_count(n):
@@ -31,65 +27,3 @@ def pi_count(n):
                 if x < p*p: break
                 dp[i] -= dp[Q_idx(x//p)] - tmp
     return dp[1]
-
-def fastest_count_primes(n):
-    if n < 2:
-        return 0
-    v = int(n ** 0.5) + 1
-    smalls = [i // 2 for i in range(1, v + 1)]
-    smalls[1] = 0
-    s = v // 2
-    roughs = [2 * i + 1 for i in range(s)]
-    larges = [(n // (2 * i + 1) + 1) // 2 for i in range(s)]
-    skip = [False] * v
-
-    pc = 0
-    for p in range(3, v):
-        if smalls[p] <= smalls[p - 1]:
-            continue
-        else:
-            print(p)
-        
-        q = p * p
-        pc += 1 #n^(1/4)以下の素数の個数
-        if q * q > n:
-            break
-        # 素数じゃないフラグ
-        skip[p] = True
-        for i in range(q, v, 2 * p):
-            skip[i] = True
-            
-        ns = 0
-        for k in range(s):
-            i = roughs[k]
-            if skip[i]:
-                continue
-            d = i * p
-            larges[ns] = larges[k] - (larges[smalls[d] - pc] if d < v else smalls[n // d]) + pc
-            roughs[ns] = i
-            ns += 1
-        print(roughs)
-        
-        # ふるわれる個数のけいさんがらくだね
-        s = ns
-        for j in range((v - 1) // p, p - 1, -1):
-            c = smalls[j] - pc
-            e = min((j + 1) * p, v)
-            for i in range(j * p, e):
-                smalls[i] -= c
-                print(i,c)
-
-    for k in range(1, s):
-        m = n // roughs[k]
-        s = larges[k] - (pc + k - 1)
-        for l in range(1, k):
-            p = roughs[l]
-            if p * p > m:
-                break
-            s -= smalls[m // p] - (pc + l - 1)
-        larges[0] -= s
-
-    return larges[0]
-
-
-fastest_count_primes(1000)
